0755821f58fb95c0c844406265a4ee12637fd18b
Penhaldev <penhaldev@gmail.com>
2023-10-30 21:37:32 -0500

    Map Drawing

--- draw_map.py
+++ draw_map.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 
+#Map drawing, receives dataframe as parameter
 def draw_map(df):
     fig = px.scatter_mapbox(df, lon = df['LON'], lat = df['LAT'], zoom = 10, color = df['Color'] , size = df['Size'], width=900 , height=600 ,title='DIRECTIONS MAP')
     fig.update_layout(mapbox_style = "open-street-map")
